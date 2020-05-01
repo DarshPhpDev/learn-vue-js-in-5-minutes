9bb786d8ea68a9f4af4eea9caeb525edde5a6095
mustafashifters <mustafa.ahmed@shifters.tech>
2020-05-01 17:39:10 +0200

    Attribute Binding

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-// 1- Import Vue Cdn script.
-// 2- Use Vue devtools.
 
 var app = new Vue({
     el:'#app',
     data:{
-        title:'Ramadan Kareem'
+        link:'Ramadan Kareem',
+        imgUrl:'./assets/'
     }
 });
